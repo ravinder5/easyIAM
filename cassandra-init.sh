df1ab5d882aef8cf39816614d4c4ed0fd00f701f
ravinder <ravinder.reddy54@gmail.com>
2019-12-03 16:25:16 -0600

    Base classes for authorization

--- cassandra-init.sh
+++ cassandra-init.sh
@@ -1,4 +1,4 @@
-CQL="CREATE KEYSPACE IF NOT EXISTS ravi_iam WITH replication = {'class': 'SimpleStrategy', 'replication_factor':'1'};
+CQL="CREATE KEYSPACE IF NOT EXISTS easy_iam WITH replication = {'class': 'SimpleStrategy', 'replication_factor':'1'};
 CREATE TABLE easy_iam.authentication (user_name text, user_id text, password text, PRIMARY KEY (user_name));
 CREATE TABLE easy_iam.clientConfig (client_id text, client_secret text, scope text, PRIMARY KEY (client_id));
 CREATE TABLE easy_iam.authCookie (auth_cookie_id text, isAuthenticated boolean, client_id text, user_name text, user_id text, PRIMARY KEY (auth_cookie_id));
